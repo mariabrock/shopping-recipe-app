837146c38b6817431dc5763666466ea0ed2a730d
Maria Brock <brockm@ornl.giv>
2023-02-03 17:34:19 -0500

    added components needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingComponent } from './components/shopping/shopping.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { ListComponent } from './components/shopping/list/list.component';
+import { ListEditComponent } from './components/shopping/list-edit/list-edit.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ItemComponent } from './components/recipes/item/item.component';
+import { DetailComponent } from './components/recipes/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingComponent,
+    RecipesComponent,
+    ListComponent,
+    ListEditComponent,
+    HeaderComponent,
+    ItemComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
