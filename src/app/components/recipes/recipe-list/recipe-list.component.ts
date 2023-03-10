0b38f31f56c11af12004b4d3a0d8088eee5580ed
Maria Brock <brockm@ornl.giv>
2023-03-10 16:51:24 -0500

    this is just for funzies

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -14,7 +14,7 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe', 'This is a test.',
       'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2053&q=80'),
     new Recipe('Another Test Recipe', 'This is a second test.',
-      'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2053&q=80')
+      'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1081&q=80')
   ];
 
   constructor() { }
