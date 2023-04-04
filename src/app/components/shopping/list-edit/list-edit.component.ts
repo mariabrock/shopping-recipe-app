b722a124dbda53d1db63e8dca5ae2dcebf4820b7
Maria Brock <brockm@ornl.giv>
2023-04-04 16:58:50 -0400

    added some code that I missed

--- src/app/components/shopping/list-edit/list-edit.component.ts
+++ src/app/components/shopping/list-edit/list-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ShoppingListService} from "../shopping-list.service";
+import {Ingredient} from "../../../shared/ingredient.model";
 
 @Component({
   selector: 'app-list-edit',
@@ -7,6 +8,8 @@ import {ShoppingListService} from "../shopping-list.service";
   styleUrls: ['./list-edit.component.css']
 })
 export class ListEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -14,6 +17,9 @@ export class ListEditComponent implements OnInit {
   }
 
   onAddItem() {
-
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.slService.addIngredient(newIngredient);
   }
 }
