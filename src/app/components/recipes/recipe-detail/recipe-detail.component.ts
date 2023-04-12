c7a2972a27d3621d2de7ac3d87d1730639334ec6
Maria Brock <brockm@ornl.giv>
2023-04-12 11:01:06 -0400

    completed routing

--- src/app/components/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, OnInit, } from '@angular/core';
 import { Recipe} from "../recipe.model";
 import { RecipeService } from "../recipe.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,7 +13,8 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.route.params
@@ -28,5 +29,11 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList() {
   this.recipeService.addIngredientsToList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+    // this is a demo of what a more complex route might look like
   }
 }
